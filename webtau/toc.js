f0b8cd5fd8c407a19f8f6b8ed5836cede1e82efd
tsiq-nick <nick.golubyev@twosigmaiq.com>
2018-05-03 22:30:43 -0400

    initial docs

---
+++ webtau/toc.js
@@ -0,0 +1,243 @@
+toc = [
+  {
+    "sectionTitle": "",
+    "dirName": "",
+    "items": [
+      {
+        "sectionTitle": "",
+        "pageTitle": "Index",
+        "pageMeta": {},
+        "fileName": "index",
+        "dirName": "",
+        "pageSectionIdTitles": [
+          {
+            "title": "WebTau",
+            "id": "webtau"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "sectionTitle": "REST",
+    "dirName": "REST",
+    "items": [
+      {
+        "sectionTitle": "REST",
+        "pageTitle": "Getting Started",
+        "pageMeta": {},
+        "fileName": "getting-started",
+        "dirName": "REST",
+        "pageSectionIdTitles": [
+          {
+            "title": "Installation",
+            "id": "installation"
+          },
+          {
+            "title": "Bare Minimum",
+            "id": "bare-minimum"
+          },
+          {
+            "title": "Config File",
+            "id": "config-file"
+          }
+        ]
+      },
+      {
+        "sectionTitle": "REST",
+        "pageTitle": "CRUD",
+        "pageMeta": {},
+        "fileName": "CRUD",
+        "dirName": "REST",
+        "pageSectionIdTitles": [
+          {
+            "title": "Example",
+            "id": "example"
+          },
+          {
+            "title": "Report",
+            "id": "report"
+          },
+          {
+            "title": "Spring Boot",
+            "id": "spring-boot"
+          }
+        ]
+      },
+      {
+        "sectionTitle": "REST",
+        "pageTitle": "Headers",
+        "pageMeta": {},
+        "fileName": "headers",
+        "dirName": "REST",
+        "pageSectionIdTitles": [
+          {
+            "title": "Request Header Provider",
+            "id": "request-header-provider"
+          }
+        ]
+      },
+      {
+        "sectionTitle": "REST",
+        "pageTitle": "Documentation",
+        "pageMeta": {},
+        "fileName": "documentation",
+        "dirName": "REST",
+        "pageSectionIdTitles": [
+          {
+            "title": "Scenarios",
+            "id": "scenarios"
+          },
+          {
+            "title": "Test Artifacts",
+            "id": "test-artifacts"
+          },
+          {
+            "title": "Document REST calls",
+            "id": "document-rest-calls"
+          }
+        ]
+      },
+      {
+        "sectionTitle": "REST",
+        "pageTitle": "Complex Types",
+        "pageMeta": {},
+        "fileName": "complex-types",
+        "dirName": "REST",
+        "pageSectionIdTitles": [
+          {
+            "title": "List Of Objects",
+            "id": "list-of-objects"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "sectionTitle": "UI",
+    "dirName": "UI",
+    "items": [
+      {
+        "sectionTitle": "UI",
+        "pageTitle": "Configuration",
+        "pageMeta": {},
+        "fileName": "configuration",
+        "dirName": "UI",
+        "pageSectionIdTitles": [
+          {
+            "title": "Base URL",
+            "id": "base-url"
+          },
+          {
+            "title": "Timeouts",
+            "id": "timeouts"
+          },
+          {
+            "title": "Window Size",
+            "id": "window-size"
+          },
+          {
+            "title": "Documentation Artifacts",
+            "id": "documentation-artifacts"
+          }
+        ]
+      },
+      {
+        "sectionTitle": "UI",
+        "pageTitle": "Finders And Filters",
+        "pageMeta": {},
+        "fileName": "finders-and-filters",
+        "dirName": "UI",
+        "pageSectionIdTitles": [
+          {
+            "title": "Filters",
+            "id": "filters"
+          }
+        ]
+      },
+      {
+        "sectionTitle": "UI",
+        "pageTitle": "Page Object",
+        "pageMeta": {},
+        "fileName": "page-object",
+        "dirName": "UI",
+        "pageSectionIdTitles": [
+          {
+            "title": "Test Encapsulation",
+            "id": "test-encapsulation"
+          },
+          {
+            "title": "Definition",
+            "id": "definition"
+          },
+          {
+            "title": "Grouping",
+            "id": "grouping"
+          }
+        ]
+      },
+      {
+        "sectionTitle": "UI",
+        "pageTitle": "Page Element",
+        "pageMeta": {},
+        "fileName": "page-element",
+        "dirName": "UI",
+        "pageSectionIdTitles": [
+          {
+            "title": "Methods",
+            "id": "methods"
+          },
+          {
+            "title": "Assertions",
+            "id": "assertions"
+          }
+        ]
+      },
+      {
+        "sectionTitle": "UI",
+        "pageTitle": "Matchers",
+        "pageMeta": {},
+        "fileName": "matchers",
+        "dirName": "UI",
+        "pageSectionIdTitles": [
+          {
+            "title": "Values",
+            "id": "values"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "sectionTitle": "Configuration",
+    "dirName": "configuration",
+    "items": [
+      {
+        "sectionTitle": "Configuration",
+        "pageTitle": "Cli",
+        "pageMeta": {},
+        "fileName": "cli",
+        "dirName": "configuration",
+        "pageSectionIdTitles": [
+          {
+            "title": "Overrides",
+            "id": "overrides"
+          }
+        ]
+      },
+      {
+        "sectionTitle": "Configuration",
+        "pageTitle": "Environments",
+        "pageMeta": {},
+        "fileName": "environments",
+        "dirName": "configuration",
+        "pageSectionIdTitles": [
+          {
+            "title": "Select",
+            "id": "select"
+          }
+        ]
+      }
+    ]
+  }
+]
