b0982ad891a72ac4bc7aff2fc63a3fd4e2cad529
MykolaGolubyev <golubev.nikolay@gmail.com>
2018-06-11 10:26:08 -0400

    fix reactjs build warnings to make travis happy

--- webtau-reactjs/src/report/details/Screenshot.js
+++ webtau-reactjs/src/report/details/Screenshot.js
@@ -19,7 +19,7 @@ import React from 'react'
 const Screenshot = ({test}) => {
     return (
         <div className="image">
-            <img src={"data:image/png;base64," + test.screenshot} width="100%"/>
+            <img alt="screenshot" src={"data:image/png;base64," + test.screenshot} width="100%"/>
         </div>
     )
 }
