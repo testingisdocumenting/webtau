3072cd0c88c03d06db37d77dd2eb8034baeadf71
MykolaGolubyev <golubev.nikolay@gmail.com>
2018-07-18 20:10:02 -0400

    webtau maven plugin

---
+++ webtau-maven-plugin/src/main/groovy/com/twosigma/webtau/maven/WebTauMavenRunner.groovy
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 TWO SIGMA OPEN SOURCE, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.twosigma.webtau.maven
+
+import com.twosigma.webtau.cli.WebTauCliApp
+import org.apache.maven.plugin.AbstractMojo
+import org.apache.maven.plugin.MojoExecutionException
+import org.apache.maven.plugin.MojoFailureException
+import org.apache.maven.plugins.annotations.Mojo
+import org.apache.maven.plugins.annotations.Parameter
+import org.apache.maven.shared.model.fileset.FileSet
+import org.apache.maven.shared.model.fileset.util.FileSetManager
+
+@Mojo(name = "run")
+class WebTauMavenRunner extends AbstractMojo {
+    @Parameter
+    private FileSet tests
+
+    @Parameter
+    private String env
+
+    @Parameter
+    private String url
+
+    @Parameter
+    private String workingDir
+
+    @Override
+    void execute() throws MojoExecutionException, MojoFailureException {
+        def fileSetManager = new FileSetManager()
+        def files = fileSetManager.getIncludedFiles(tests) as List
+
+        getLog().info("test files:\n    " + files.join("\n    "))
+
+        def args = buildArgs([env: env, url: url, workingDir: workingDir])
+        args.addAll(files)
+
+        def cli = new WebTauCliApp(args as String[])
+        cli.start(true)
+    }
+
+    static List<String> buildArgs(Map params) {
+        return params.entrySet()
+                .findAll { e -> e.value != null}
+                .collect { e -> "--${e.key}=${e.value}"}
+    }
+}
