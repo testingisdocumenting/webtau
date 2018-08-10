6e794348f3c4fc4d092537e1e71f1a42415173b9
MykolaGolubyev <golubev.nikolay@gmail.com>
2018-08-10 06:48:29 -0400

    add tabs to overall summary view. initial performance tab

--- webtau-reactjs/src/report/summary/OverallSummary.js
+++ webtau-reactjs/src/report/summary/OverallSummary.js
@@ -20,16 +20,63 @@ import CallsTiming from './CallsTiming'
 import HttpOperationCoverageSummary from './HttpOperationCoverageSummary'
 import ConfigTable from './ConfigTable'
 
+import TabSelection from '../widgets/TabSelection'
+
+import PercentilePerformanceTable from './PercentilePerformanceTable'
+import PercentilePerformanceDiagram from './PercentilePerformanceDiagram'
+
 import './OverallSummary.css'
 
-function OverallSummary({report, onSwitchToHttpCalls, onSwitchToSkippedHttpCalls}) {
-    return (
-        <div className="overall-summary">
-            <CallsTiming report={report} onSwitchToHttpCalls={onSwitchToHttpCalls}/>
-            <HttpOperationCoverageSummary report={report} onSwitchToSkippedHttpCalls={onSwitchToSkippedHttpCalls}/>
-            <ConfigTable report={report}/>
-        </div>
-    )
+const summaryTabName = 'Summary'
+const configurationTabName = 'Configuration'
+const performanceTabName = 'Performance'
+const tabNames = [summaryTabName, configurationTabName, performanceTabName]
+
+export default class OverallSummary extends React.Component {
+    render() {
+        const {onTabSelection, selectedTabName = tabNames[0]} = this.props
+
+        return (
+            <div className="overall-summary">
+                <TabSelection tabs={tabNames} selectedTabName={selectedTabName} onTabSelection={onTabSelection}/>
+
+                <div className="overall-summary-tab-content">
+                    {this.renderTabContent()}
+                </div>
+            </div>
+        )
+    }
+
+    renderTabContent() {
+        const {
+            selectedTabName = tabNames[0],
+            report,
+            onSwitchToHttpCalls,
+            onSwitchToSkippedHttpCalls,
+        } = this.props
+
+        switch (selectedTabName) {
+            case summaryTabName: return (
+                <React.Fragment>
+                    <CallsTiming report={report} onSwitchToHttpCalls={onSwitchToHttpCalls}/>
+                    <HttpOperationCoverageSummary report={report} onSwitchToSkippedHttpCalls={onSwitchToSkippedHttpCalls}/>
+                </React.Fragment>
+            )
+
+            case configurationTabName: return (
+                <ConfigTable report={report}/>
+            )
+
+            case performanceTabName: return (
+                <div className="overall-summary-performance-grid">
+                    <PercentilePerformanceTable report={report}/>
+                    <PercentilePerformanceDiagram report={report}/>
+                </div>
+            )
+
+            default:
+                return null
+        }
+    }
 }
 
-export default OverallSummary
---
+++ webtau-reactjs/src/report/summary/PercentilePerformanceDiagram.js
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 TWO SIGMA OPEN SOURCE, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react'
+import {scaleLinear} from 'd3-scale'
+
+import './PercentilePerformanceDiagram.css'
+import Card from '../widgets/Card'
+
+const width = 800
+const height = 600
+
+export default class PercentilePerformanceDiagram extends React.Component {
+    render() {
+        const {report} = this.props
+
+        const performance = report.performance
+        const httpCalls = performance.sortedNotFailedHttpCalls
+
+        const percentile = performance.percentile[75]
+        const latencyAxis = scaleLinear().domain([httpCalls[0].elapsedTime, percentile.value]).range([httpCalls[0].elapsedTime, height])
+        const callIdxAxis = scaleLinear().domain([0, percentile.idx]).range([0, width])
+
+        const reducedHttpCalls = httpCalls.slice(0, percentile.idx)
+        return (
+            <div>
+                <Card width={width}>
+                    <Diagram maxX={httpCalls.length} maxY={performance.maxLatency}>
+                        {reducedHttpCalls.map((httpCall, idx) => <Point key={idx}
+                                                                         x={callIdxAxis(idx)}
+                                                                         y={latencyAxis(httpCall.elapsedTime)}/>)}
+                    </Diagram>
+                </Card>
+            </div>
+        )
+    }
+}
+
+function Diagram({maxX, maxY, children}) {
+    return (
+        <svg width={width} height={height}>
+            {children}
+        </svg>
+    )
+}
+
+function Point({x, y}) {
+    return <rect width={5} height={5} x={x - 2} y={height - y - 2} stroke="#333" strokeWidth={1} fill="#888"/>
+}
