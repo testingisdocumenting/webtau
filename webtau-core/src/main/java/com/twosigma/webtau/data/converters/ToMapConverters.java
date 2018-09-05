8ccbef8d7727b9d43fb8d843ea44542599d3fb85
MykolaGolubyev <golubev.nikolay@gmail.com>
2018-09-05 18:14:31 -0400

    add string and number compareTo handler

--- webtau-core/src/main/java/com/twosigma/webtau/data/converters/ToMapConverters.java
+++ webtau-core/src/main/java/com/twosigma/webtau/data/converters/ToMapConverters.java
@@ -18,24 +18,14 @@ package com.twosigma.webtau.data.converters;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import com.twosigma.webtau.utils.ServiceUtils;
-import com.twosigma.webtau.utils.TraceUtils;
 
 public class ToMapConverters {
     private static List<ToMapConverter> converters = discover();
 
     public static Map<String, ?> convert(Object v) {
-        if (v == null) {
-            return null;
-        }
-
-        return converters.stream().
-                map(h -> h.convert(v)).
-                filter(Objects::nonNull).
-                findFirst().
-                orElseThrow(() -> new IllegalArgumentException("can't find a map converter for: " + TraceUtils.renderValueAndType(v)));
+        return TypeConvertersUtils.convertAndThrow(converters.stream(), "map", v);
     }
 
     private static List<ToMapConverter> discover() {
