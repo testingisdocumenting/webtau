3d7bdacaf9764bf2b85bd3832a2c0200bfd8057a
MykolaGolubyev <golubev.nikolay@gmail.com>
2018-09-05 18:14:31 -0400

    add string and number compareTo handler

---
+++ webtau-core/src/main/java/com/twosigma/webtau/expectation/equality/handlers/NumberAndStringCompareToHandler.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018 TWO SIGMA OPEN SOURCE, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.twosigma.webtau.expectation.equality.handlers;
+
+import com.twosigma.webtau.expectation.ActualPath;
+import com.twosigma.webtau.expectation.equality.CompareToComparator;
+import com.twosigma.webtau.expectation.equality.CompareToHandler;
+import com.twosigma.webtau.utils.NumberUtils;
+
+public class NumberAndStringCompareToHandler implements CompareToHandler {
+    @Override
+    public boolean handleEquality(Object actual, Object expected) {
+        return handles(actual, expected);
+    }
+
+    @Override
+    public boolean handleGreaterLessEqual(Object actual, Object expected) {
+        return handles(actual, expected);
+    }
+
+    @Override
+    public void compareEqualOnly(CompareToComparator comparator, ActualPath actualPath, Object actual, Object expected) {
+        comparator.compareUsingEqualOnly(actualPath, convertToNumber(actual), expected);
+    }
+
+    @Override
+    public void compareGreaterLessEqual(CompareToComparator comparator, ActualPath actualPath, Object actual, Object expected) {
+        comparator.compareUsingCompareTo(actualPath, convertToNumber(actual), expected);
+    }
+
+    private Number convertToNumber(Object actual) {
+        return NumberUtils.convertStringToNumber((String) actual);
+    }
+
+    private boolean handles(Object actual, Object expected) {
+        return actual instanceof String &&
+                expected instanceof Number;
+    }
+}
