50d0beeecc802f1dd11c9bf46e713da90602a883
MykolaGolubyev <golubev.nikolay@gmail.com>
2019-04-20 18:55:13 -0400

    moved to src/main/groovy

---
+++ webtau-report-testing/src/test/groovy/pages/ReportLocation.groovy
@@ -0,0 +1,15 @@
+package pages
+
+import static com.twosigma.webtau.WebTauDsl.getCfg
+
+class ReportLocation {
+    static String fullUrl(String reportName) {
+        return 'file://' + cfg.workingDir.toAbsolutePath()
+                .resolve('..')
+                .resolve('..')
+                .resolve('..')
+                .resolve('..')
+                .resolve('webtau-feature-testing')
+                .resolve(reportName).toAbsolutePath()
+    }
+}
