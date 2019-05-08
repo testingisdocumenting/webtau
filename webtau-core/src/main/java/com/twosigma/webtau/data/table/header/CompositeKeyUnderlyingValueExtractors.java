d1046661a97a78d1a0b6462f31d23bb9f0f6711d
MykolaGolubyev <golubev.nikolay@gmail.com>
2019-05-08 17:54:53 -0400

    merge mastermerge master

--- webtau-core/src/main/java/com/twosigma/webtau/data/table/header/CompositeKeyUnderlyingValueExtractors.java
+++ webtau-core/src/main/java/com/twosigma/webtau/data/table/header/CompositeKeyUnderlyingValueExtractors.java
@@ -3,11 +3,10 @@ package com.twosigma.webtau.data.table.header;
 import com.twosigma.webtau.utils.ServiceLoaderUtils;
 import com.twosigma.webtau.utils.TraceUtils;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.List;
 
 public class CompositeKeyUnderlyingValueExtractors {
-    private static final Set<CompositeKeyUnderlyingValueExtractor> extractors = discover();
+    private static final List<CompositeKeyUnderlyingValueExtractor> extractors = discover();
 
     public static Object extract(Object value) {
         return extractors.stream()
@@ -17,9 +16,9 @@ public class CompositeKeyUnderlyingValueExtractors {
                 .extract(value);
     }
 
-    private static Set<CompositeKeyUnderlyingValueExtractor> discover() {
-        Set<CompositeKeyUnderlyingValueExtractor> result =
-                new LinkedHashSet<>(ServiceLoaderUtils.load(CompositeKeyUnderlyingValueExtractor.class));
+    private static List<CompositeKeyUnderlyingValueExtractor> discover() {
+        List<CompositeKeyUnderlyingValueExtractor> result =
+                ServiceLoaderUtils.load(CompositeKeyUnderlyingValueExtractor.class);
         result.add(new CompositeKeyDefaultUnderlyingValueExtractor());
 
         return result;
