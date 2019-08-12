ec015eeff3692ed4409920c4e670308eb6bf4b21
MykolaGolubyev <golubev.nikolay@gmail.com>
2019-08-11 21:01:12 -0400

    remove unused examples

--- webtau-feature-testing/examples/scenarios/ui/SearchWithWait.groovy
+++
@@ -1,9 +0,0 @@
-package scenarios.ui
-
-import static com.twosigma.webtau.WebTauGroovyDsl.*
-import static pages.Pages.*
-
-scenario("search by specific query") {
-    search.submit(query: "search this")
-    search.numberOfResults.waitTo == 2
-}
--- webtau-feature-testing/examples/scenarios/ui/shouldNot.groovy
+++
@@ -1,9 +0,0 @@
-package scenarios.ui
-
-import static com.twosigma.webtau.WebTauGroovyDsl.*
-import static pages.Pages.*
-
-scenario("Executes negated matcher against page element's underlying value") {
-    search.open()
-    search.welcomeMessage.shouldNot == 'welcome to boring search'
-}
