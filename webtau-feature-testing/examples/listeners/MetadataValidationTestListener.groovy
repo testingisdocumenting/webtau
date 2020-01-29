b83b52e993c31f4a77fd86efdbd4428b8759f33f
Mykola <golubev.nikolay@gmail.com>
2020-01-29 17:03:15 -0500

    MetaDataValidationTestListener -> MetadataValidationTestListener

--- webtau-feature-testing/examples/listeners/MetadataValidationTestListener.groovy
+++ webtau-feature-testing/examples/listeners/MetadataValidationTestListener.groovy
@@ -3,7 +3,7 @@ package listeners
 import com.twosigma.webtau.reporter.TestListener
 import com.twosigma.webtau.reporter.WebTauTest
 
-class MetaDataValidationTestListener implements TestListener {
+class MetadataValidationTestListener implements TestListener {
     @Override
     void afterLastTestStatement(WebTauTest test) {
         if (!test.metadata.has('owner')) {
