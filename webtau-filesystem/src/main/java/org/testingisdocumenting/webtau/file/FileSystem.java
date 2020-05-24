5cddee5b5cdc134f83bdbc5255b537047f64010e
Mykola <golubev.nikolay@gmail.com>
2020-05-24 18:03:19 -0400

    add fs.tempDir. fs.unzip is a test step

--- webtau-filesystem/src/main/java/org/testingisdocumenting/webtau/file/FileSystem.java
+++ webtau-filesystem/src/main/java/org/testingisdocumenting/webtau/file/FileSystem.java
@@ -16,11 +16,19 @@
 
 package org.testingisdocumenting.webtau.file;
 
+import org.apache.commons.io.FileUtils;
 import org.testingisdocumenting.webtau.file.zip.UnzipTask;
+import org.testingisdocumenting.webtau.reporter.StepReportOptions;
+import org.testingisdocumenting.webtau.reporter.TestStep;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.testingisdocumenting.webtau.cfg.WebTauConfig.getCfg;
+import static org.testingisdocumenting.webtau.reporter.IntegrationTestsMessageBuilder.*;
+import static org.testingisdocumenting.webtau.reporter.TokenizedMessage.tokenizedMessage;
 
 public class FileSystem {
     public static final FileSystem fs = new FileSystem();
@@ -29,8 +37,34 @@ public class FileSystem {
     }
 
     public void unzip(Path src, Path dest) {
-        UnzipTask unzipTask = new UnzipTask(fullPath(src), fullPath(dest));
-        unzipTask.execute();
+        TestStep<Object, Void> step = TestStep.createStep(null,
+                tokenizedMessage(action("unzipping "), urlValue(src.toString()), TO, urlValue(dest.toString())),
+                () -> tokenizedMessage(action("unzipped "), urlValue(src.toString()), TO, urlValue(dest.toString())),
+                () -> {
+                    UnzipTask unzipTask = new UnzipTask(fullPath(src), fullPath(dest));
+                    unzipTask.execute();
+                });
+
+        step.execute(StepReportOptions.REPORT_ALL);
+    }
+
+    public void unzip(String src, Path dest) {
+        unzip(Paths.get(src), dest);
+    }
+
+    public void unzip(String src, String dest) {
+        unzip(Paths.get(src), Paths.get(dest));
+    }
+
+    public Path tempDir(String prefix) {
+        try {
+            Path path = Files.createTempDirectory(prefix);
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> FileUtils.deleteQuietly(path.toFile())));
+
+            return path;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static Path fullPath(Path relativeOrFull) {
