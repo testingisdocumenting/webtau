006b0987c59f5559a42cf4665f893e35353f9f48
Mykola <golubev.nikolay@gmail.com>
2020-06-28 15:56:44 -0400

    add browser cli sanity check

---
+++ webtau-cli-testing/src/test/groovy/scenarios/testscripts.groovy
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2020 webtau maintainers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package scenarios
+
+import static org.testingisdocumenting.webtau.WebTauGroovyDsl.*
+import static webtau.CliCommands.*
+
+scenario('browserSanity') {
+    webtauCli.run("testscripts/browserSanity.groovy " +
+            "--workingDir=${cfg.workingDir} " +
+            "--reportPath=webtau.browser-sanity.report.html")
+}
