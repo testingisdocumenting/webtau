628ffa3639eb177a0af7e57325684392e2c5c256
Mykola <golubev.nikolay@gmail.com>
2020-07-16 20:12:26 -0400

    http query parameters encoding + docs

--- webtau-http/src/test/java/org/testingisdocumenting/webtau/http/HttpTestDataServer.java
+++ webtau-http/src/test/java/org/testingisdocumenting/webtau/http/HttpTestDataServer.java
@@ -81,6 +81,7 @@ public class HttpTestDataServer {
         testServer.registerDelete("/resource", new TestServerTextResponse("abc"));
         testServer.registerGet("/params?a=1&b=text", new TestServerJsonResponse("{\"a\": 1, \"b\": \"text\"}"));
         testServer.registerPost("/params?a=1&b=text", new TestServerJsonResponse("{\"a\": 1, \"b\": \"text\"}", 201));
+        testServer.registerGet("/params?message=hello+world+%21", new TestServerJsonResponse("{}", 200));
         testServer.registerGet("/integer", new TestServerJsonResponse("123"));
         testServer.registerPost("/json-derivative", new TestServerJsonDerivativeResponse());
 
