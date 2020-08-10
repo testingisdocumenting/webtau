bb59da5bb786604c0bdc35daf5b1193384a40f56
Karol Dudzinski <karol.dudzinski@twosigmaiq.com>
2020-08-10 21:58:10 +0100

    rename field

--- webtau-graphql/src/main/java/org/testingisdocumenting/webtau/graphql/GraphQLReportDataProvider.java
+++ webtau-graphql/src/main/java/org/testingisdocumenting/webtau/graphql/GraphQLReportDataProvider.java
@@ -33,23 +33,23 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class GraphQLReportDataProvider implements ReportDataProvider {
-    private final Supplier<GraphQLCoverage> coverage;
+    private final Supplier<GraphQLCoverage> coverageSupplier;
 
     public GraphQLReportDataProvider() {
         this(GraphQL::getCoverage);
     }
 
-    public GraphQLReportDataProvider(Supplier<GraphQLCoverage> coverage) {
-        this.coverage = coverage;
+    public GraphQLReportDataProvider(Supplier<GraphQLCoverage> coverageSupplier) {
+        this.coverageSupplier = coverageSupplier;
     }
 
     @Override
     public Stream<ReportCustomData> provide(WebTauTestList tests) {
-        List<? extends Map<String, ?>> nonCoveredOperations = coverage.get().nonCoveredOperations()
+        List<? extends Map<String, ?>> nonCoveredOperations = coverageSupplier.get().nonCoveredOperations()
                 .map(GraphQLOperation::toMap)
                 .collect(Collectors.toList());
 
-        List<? extends Map<String, ?>> coveredOperations = coverage.get().coveredOperations()
+        List<? extends Map<String, ?>> coveredOperations = coverageSupplier.get().coveredOperations()
                 .map(GraphQLOperation::toMap)
                 .collect(Collectors.toList());
 
@@ -65,7 +65,7 @@ public class GraphQLReportDataProvider implements ReportDataProvider {
     }
 
     private List<? extends Map<String, ?>> computeTiming() {
-        return coverage.get().actualCalls().map(GraphQLReportDataProvider::computeTiming).collect(Collectors.toList());
+        return coverageSupplier.get().actualCalls().map(GraphQLReportDataProvider::computeTiming).collect(Collectors.toList());
     }
 
     private static Map<String, ?> computeTiming(Map.Entry<GraphQLOperation, Set<GraphQLCoveredOperations.Call>> entry) {
@@ -91,8 +91,8 @@ public class GraphQLReportDataProvider implements ReportDataProvider {
 
     private Map<String, ?> computeCoverageSummary() {
         Map<String, Object> summary = new HashMap<>();
-        Map<GraphQLOperationType, List<GraphQLOperation>> declaredOpByType = coverage.get().declaredOperations().collect(Collectors.groupingBy(GraphQLOperation::getType));
-        Map<GraphQLOperationType, List<GraphQLOperation>> coveredOpsByType = coverage.get().coveredOperations().collect(Collectors.groupingBy(GraphQLOperation::getType));
+        Map<GraphQLOperationType, List<GraphQLOperation>> declaredOpByType = coverageSupplier.get().declaredOperations().collect(Collectors.groupingBy(GraphQLOperation::getType));
+        Map<GraphQLOperationType, List<GraphQLOperation>> coveredOpsByType = coverageSupplier.get().coveredOperations().collect(Collectors.groupingBy(GraphQLOperation::getType));
 
         Map<String, Object> summaryByType = new HashMap<>();
         declaredOpByType.forEach((type, ops) -> {
