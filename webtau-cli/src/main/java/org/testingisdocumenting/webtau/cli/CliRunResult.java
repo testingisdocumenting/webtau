27e95e58a47fb82b56c43b0b0c010af7cbf8e99c
Mykola <golubev.nikolay@gmail.com>
2020-08-26 20:11:03 -0400

    cli: expose run result as return param

---
+++ webtau-cli/src/main/java/org/testingisdocumenting/webtau/cli/CliRunResult.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 webtau maintainers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.testingisdocumenting.webtau.cli;
+
+public class CliRunResult {
+    private final int exitCode;
+    private final String output;
+    private final String error;
+
+    public CliRunResult(int exitCode, String output, String error) {
+        this.exitCode = exitCode;
+        this.output = output;
+        this.error = error;
+    }
+
+    public int getExitCode() {
+        return exitCode;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
---
+++ webtau-cli/src/test/java/org/testingisdocumenting/webtau/cli/CliCommandJavaTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 webtau maintainers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.testingisdocumenting.webtau.cli;
+
+import org.junit.Test;
+
+import static org.testingisdocumenting.webtau.Matchers.*;
+import static org.testingisdocumenting.webtau.cli.Cli.cli;
+import static org.testingisdocumenting.webtau.cli.CliTestUtils.supportedPlatformOnly;
+
+public class CliCommandJavaTest {
+    private static final CliCommand ls = cli.command("ls -l");
+    private static final CliCommand wrongLs = cli.command("a_ls -l");
+
+    @Test
+    public void runResultNoValidation() {
+        supportedPlatformOnly(() -> {
+            CliRunResult result = ls.run();
+
+            actual(result.getExitCode()).should(equal(0));
+            actual(result.getError()).should(equal(""));
+            actual(result.getOutput()).should(contain("pom.xml"));
+        });
+    }
+
+    @Test
+    public void runResultAndValidation() {
+        supportedPlatformOnly(() -> {
+            CliRunResult result = wrongLs.run(((exitCode, output, error) -> exitCode.should(equal(127))));
+
+            actual(result.getExitCode()).should(equal(127));
+            actual(result.getError()).should(contain("command not found"));
+            actual(result.getOutput()).should(equal(""));
+        });
+    }
+}
