82dd2b9e9bd9a69c72360eb53aa8e904521935bd
Mykola <golubev.nikolay@gmail.com>
2020-12-09 20:35:44 -0500

    groovy-runner: print version and build date

--- webtau-groovy/src/main/groovy/org/testingisdocumenting/webtau/cfg/WebTauCliArgsConfig.groovy
+++ webtau-groovy/src/main/groovy/org/testingisdocumenting/webtau/cfg/WebTauCliArgsConfig.groovy
@@ -23,6 +23,7 @@ import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.HelpFormatter
 import org.apache.commons.cli.Options
 import org.apache.commons.cli.ParseException
+import org.testingisdocumenting.webtau.version.WebtauVersion
 
 import java.nio.file.Paths
 
@@ -95,8 +96,8 @@ class WebTauCliArgsConfig {
     private void printHelp(Options options) {
         HelpFormatter helpFormatter = new HelpFormatter()
 
-        def header = "version: " + WebTauMeta.version
-        helpFormatter.printHelp("webtau [options] [testFile1] [testFile2]", header, options, "")
+        WebtauVersion.print()
+        helpFormatter.printHelp("webtau [options] [testFile1] [testFile2]", "", options, "")
         exitHandler.exit(1)
     }
 
