393eca365c27cbc8461a7cc65c4cc115fd735a9e
Mykola <golubev.nikolay@gmail.com>
2021-01-31 22:23:52 -0500

    fix test

--- webtau-feature-testing/examples/scenarios/db/dbProviderThroughConfig.groovy
+++ webtau-feature-testing/examples/scenarios/db/dbProviderThroughConfig.groovy
@@ -27,5 +27,5 @@ scenario('access db using provider from config') {
 """
     )
 
-    db.table("PRICES").createQuery().queryNumberOfRows().should == 0
+    db.table("PRICES").query().numberOfRows().should == 0
 }
