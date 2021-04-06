9c344d8de1f97509d643be34380e50323b7445f6
Mykola Golubyev <golubev.nikolay@gmail.com>
2021-04-06 19:03:48 -0400

    cache: accessing non cached value throws exception (#892)

---
+++ webtau-cache/src/test/groovy/org/testingisdocumenting/webtau/cache/CacheTest.groovy
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 webtau maintainers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.testingisdocumenting.webtau.cache
+
+import org.junit.Test
+
+import static org.testingisdocumenting.webtau.Matchers.code
+import static org.testingisdocumenting.webtau.Matchers.throwException
+import static org.testingisdocumenting.webtau.cache.Cache.cache
+
+class CacheTest {
+    @Test
+    void "should throw assertion error when accessing value that is not present"() {
+        code {
+            cache.get("non-existing-cache-key")
+        } should throwException("can't find cached value by key: non-existing-cache-key")
+    }
+}
