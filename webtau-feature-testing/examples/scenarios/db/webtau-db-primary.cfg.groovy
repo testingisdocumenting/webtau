8d806f23935819dc303a6a3d7e5b2975cc15f0ec
Mykola <golubev.nikolay@gmail.com>
2021-04-17 20:48:53 -0400

    modify test

--- webtau-feature-testing/examples/scenarios/db/webtau-db-primary.cfg.groovy
+++ webtau-feature-testing/examples/scenarios/db/webtau-db-primary.cfg.groovy
@@ -5,7 +5,7 @@ import groovy.grape.Grape
 Grape.grab(group:'com.h2database', module: 'h2', version: '1.4.200', // auto download DB driver dependency (in this case H2 db)
         classLoader: ClassLoader.getSystemClassLoader())
 
-dbUrl = "jdbc:h2:file:~/customerdb/h2-db" // primary DB JDBC connection url
+dbUrl = "jdbc:h2:file:~/h2db/test-db" // primary DB JDBC connection url
 dbDriverClassName = "org.h2.Driver" // JDBC driver class name
 dbUserName = "sa"
 dbPassword = "password"
