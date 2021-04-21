131b1fe833c948b4bcf864be6cbcabba9b887c82
Mykola <golubev.nikolay@gmail.com>
2021-04-21 09:54:55 -0400

    http: upgrade spring boot example

--- webtau-testapp/src/main/java/com/example/demo/springboot/app/data/RepositoryConfig.java
+++ webtau-testapp/src/main/java/com/example/demo/springboot/app/data/RepositoryConfig.java
@@ -2,11 +2,10 @@ package com.example.demo.springboot.app.data;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 @Configuration
-public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
-    @Override
+public class RepositoryConfig implements RepositoryRestConfigurer {
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Customer.class);
         config.useHalAsDefaultJsonMediaType(false);
