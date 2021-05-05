c4684f0f90740345bb1c9f2f5ee18c699af4c05f
Mykola Golubyev <golubev.nikolay@gmail.com>
2021-05-05 14:21:04 -0400

    server: initial server.serve(path) (#908)

---
+++ webtau-feature-testing/src/test/groovy/org/testingisdocumenting/webtau/featuretesting/WebTauServerFeaturesTest.groovy
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 webtau maintainers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.testingisdocumenting.webtau.featuretesting
+
+import org.junit.BeforeClass
+import org.junit.Test
+
+class WebTauServerFeaturesTest {
+    private static WebTauEndToEndTestRunner testRunner
+
+    @BeforeClass
+    static void init() {
+        testRunner = new WebTauEndToEndTestRunner()
+    }
+
+    @Test
+    void "static server"() {
+        runCli('staticContent.groovy', 'webtau-static-server.cfg.groovy')
+    }
+
+    private static void runCli(String testName, String configFileName, String... additionalArgs) {
+        testRunner.runCliWithWorkingDir("scenarios/server/$testName",
+                "examples",
+                configFileName.isEmpty() ? "" : "scenarios/server/$configFileName", additionalArgs)
+    }
+}
---
+++ webtau-server/src/main/java/org/testingisdocumenting/webtau/server/WebtauServersRegistry.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 webtau maintainers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.testingisdocumenting.webtau.server;
+
+import org.testingisdocumenting.webtau.reporter.TestListener;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class WebtauServersRegistry implements TestListener {
+    private static final Map<String, WebtauServer> serverById = new ConcurrentHashMap<>();
+
+    public static boolean hasServerWithId(String id) {
+        return serverById.containsKey(id);
+    }
+
+    public static void register(WebtauServer server) {
+        serverById.put(server.getId(), server);
+    }
+
+    public static void unregister(WebtauServer server) {
+        serverById.remove(server.getId());
+    }
+
+    @Override
+    public void afterAllTests() {
+        serverById.values().forEach(WebtauServer::stop);
+        serverById.clear();
+    }
+}
