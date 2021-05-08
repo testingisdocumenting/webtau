1bc835af5fdb46a4c96252aa6837873fab5b9000
Mykola <golubev.nikolay@gmail.com>
2021-05-08 14:02:32 -0400

    browser: page element value repl pretty print

--- webtau-browser/src/main/java/org/testingisdocumenting/webtau/browser/page/PageElementValue.java
+++ webtau-browser/src/main/java/org/testingisdocumenting/webtau/browser/page/PageElementValue.java
@@ -17,18 +17,25 @@
 
 package org.testingisdocumenting.webtau.browser.page;
 
+import org.testingisdocumenting.webtau.console.ConsoleOutput;
+import org.testingisdocumenting.webtau.console.ansi.Color;
+import org.testingisdocumenting.webtau.data.render.DataRenderers;
+import org.testingisdocumenting.webtau.data.render.PrettyPrintable;
 import org.testingisdocumenting.webtau.expectation.ActualPath;
 import org.testingisdocumenting.webtau.expectation.ActualPathAndDescriptionAware;
 import org.testingisdocumenting.webtau.expectation.ActualValueExpectations;
 import org.testingisdocumenting.webtau.reporter.IntegrationTestsMessageBuilder;
 import org.testingisdocumenting.webtau.reporter.StepReportOptions;
 import org.testingisdocumenting.webtau.reporter.TokenizedMessage;
+import org.testingisdocumenting.webtau.reporter.TokenizedMessageToAnsiConverter;
+
+import java.util.stream.Stream;
 
 import static org.testingisdocumenting.webtau.WebTauCore.*;
 import static org.testingisdocumenting.webtau.reporter.IntegrationTestsMessageBuilder.*;
 import static org.testingisdocumenting.webtau.reporter.TokenizedMessage.*;
 
-public class PageElementValue<E> implements ActualValueExpectations, ActualPathAndDescriptionAware {
+public class PageElementValue<E> implements ActualValueExpectations, ActualPathAndDescriptionAware, PrettyPrintable {
     private final ActualPathAndDescriptionAware parent;
     private final String name;
     private final PageElementValueFetcher<E> valueFetcher;
@@ -68,5 +75,21 @@ public class PageElementValue<E> implements ActualValueExpectations, ActualPathA
     @Override
     public StepReportOptions shouldReportOption() {
         return StepReportOptions.REPORT_ALL;
+    }
+
+    @Override
+    public void prettyPrint(ConsoleOutput console) {
+        console.out(
+                Stream.concat(parentPrettyPrint(),
+                        Stream.of(Color.PURPLE, name, ":", Color.GREEN, " ", DataRenderers.render(get()))).toArray());
+    }
+
+    private Stream<Object> parentPrettyPrint() {
+        if (parent == null) {
+            return Stream.empty();
+        }
+
+        TokenizedMessageToAnsiConverter toAnsiConverter = IntegrationTestsMessageBuilder.getConverter();
+        return Stream.concat(toAnsiConverter.convert(parent.describe()).stream(), Stream.of(" "));
     }
 }
