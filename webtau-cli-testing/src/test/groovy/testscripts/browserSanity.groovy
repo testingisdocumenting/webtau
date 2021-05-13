c4713b5e983bce0cb5edb105770c8f20c40f734f
Mykola <golubev.nikolay@gmail.com>
2021-05-13 18:11:25 -0400

    fix

--- webtau-cli-testing/src/test/groovy/testscripts/browserSanity.groovy
+++ webtau-cli-testing/src/test/groovy/testscripts/browserSanity.groovy
@@ -23,6 +23,5 @@ scenario('open browser and perform validation') {
     def basicHtmlPath = cfg.fullPath("data/basic.html").toAbsolutePath()
     browser.open("file://${basicHtmlPath.toAbsolutePath()}")
 
-    // TODO revert
-    $("p").should == "hello web page222"
+    $("p").should == "hello web page"
 }
