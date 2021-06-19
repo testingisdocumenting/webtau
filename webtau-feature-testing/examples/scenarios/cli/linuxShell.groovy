f841f326a1db9002e34d21a21ee776f7de805a6b
Mykola <golubev.nikolay@gmail.com>
2021-06-19 19:48:47 -0400

    debug

--- webtau-feature-testing/examples/scenarios/cli/linuxShell.groovy
+++ webtau-feature-testing/examples/scenarios/cli/linuxShell.groovy
@@ -3,6 +3,9 @@ package scenarios.cli
 import static org.testingisdocumenting.webtau.WebTauGroovyDsl.*
 
 scenario("shell source and script run") {
+    cli.run('which sh')
+    cli.run('ls /usr/bin/sh')
+
     cli.run('sh -c "source scripts/setup.sh && scripts/post_setup.sh"') {
         output.should == "my process\n" +
                 "1.23.2"
