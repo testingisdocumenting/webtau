dbf3197a4184e22d46837e6b6606ea9484bcb01a
Mykola Golubyev <golubev.nikolay@gmail.com>
2022-07-18 16:33:04 -0400

    data: doc pdf (#1195)

---
+++ webtau-data/src/main/java/org/testingisdocumenting/webtau/data/FileOrResourceBinaryDataProvider.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 webtau maintainers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.testingisdocumenting.webtau.data;
+
+import org.testingisdocumenting.webtau.utils.FileUtils;
+import org.testingisdocumenting.webtau.utils.ResourceUtils;
+
+public class FileOrResourceBinaryDataProvider implements BinaryDataProvider {
+    private final DataPath dataPath;
+
+    public FileOrResourceBinaryDataProvider(DataPath dataPath) {
+        this.dataPath = dataPath;
+    }
+
+    @Override
+    public byte[] getBinaryContent() {
+        return dataPath.isResource() ?
+                ResourceUtils.binaryContent(dataPath.getGivenPathAsString()):
+                FileUtils.fileBinaryContent(dataPath.getFullFilePath());
+    }
+
+    @Override
+    public String binaryDataSource() {
+        return null;
+    }
+}
