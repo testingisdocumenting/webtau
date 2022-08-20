c1b862d9028b60a16980c094e481f0015302e672
mykolagolubyev <golubev.nikolay@gmail.com>
2022-08-20 15:28:55 -0400

    cache: get with expiration

--- webtau-cache/src/main/java/org/testingisdocumenting/webtau/cache/Cache.java
+++ webtau-cache/src/main/java/org/testingisdocumenting/webtau/cache/Cache.java
@@ -18,12 +18,16 @@
 package org.testingisdocumenting.webtau.cache;
 
 import org.testingisdocumenting.webtau.cfg.WebTauConfig;
+import org.testingisdocumenting.webtau.reporter.MessageToken;
 import org.testingisdocumenting.webtau.reporter.StepReportOptions;
 import org.testingisdocumenting.webtau.reporter.WebTauStep;
+import org.testingisdocumenting.webtau.reporter.WebTauStepInputKeyValue;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import static org.testingisdocumenting.webtau.reporter.IntegrationTestsMessageBuilder.*;
 import static org.testingisdocumenting.webtau.reporter.IntegrationTestsMessageBuilder.stringValue;
@@ -46,12 +50,56 @@ public class Cache {
         return getAsStep(key, Function.identity());
     }
 
-    public boolean exists(String key) {
+    public <E> E get(String key, long expirationMs, Supplier<E> newValueSupplier) {
         WebTauStep step = WebTauStep.createStep(
-                tokenizedMessage(action("check"), id(key), action("cache value presence")),
-                (result) -> tokenizedMessage(action("checked"), id(key), action("cache value presence"), COLON,
+                tokenizedMessage(action("getting cached or generating new value"), FROM, id(key)),
+                (r) -> {
+                    @SuppressWarnings("unchecked")
+                    CachedValueAndMethod<E> cachedValueAndMethod = (CachedValueAndMethod<E>) r;
+
+                    MessageToken preposition = cachedValueAndMethod.method == ObtainMethod.CACHED ? FROM : AS;
+                    return tokenizedMessage(action(cachedValueAndMethod.method.message), preposition, id(key), COLON, stringValue(cachedValueAndMethod.value));
+                },
+                () -> getWithExpirationAndSupplierStep(key, expirationMs, newValueSupplier, Function.identity()));
+
+        step.setInput(WebTauStepInputKeyValue.stepInput(Collections.singletonMap("expirationMs", expirationMs)));
+
+        CachedValueAndMethod<E> executionResult = step.execute(StepReportOptions.REPORT_ALL);
+        return executionResult.value;
+
+    }
+
+    public boolean exists(String key) {
+        MessageToken valuePresenceMessage = action("cache value presence");
+        WebTauStep step = WebTauStep.createStep(
+                tokenizedMessage(action("check"), id(key), valuePresenceMessage),
+                (result) -> tokenizedMessage(action("checked"), id(key), valuePresenceMessage, COLON,
                         classifier((boolean)result ? "exists" : "absent")),
                 () -> fileBasedCache.exists(key));
+
+        return step.execute(StepReportOptions.SKIP_START);
+    }
+
+    public void remove(String key) {
+        MessageToken valueMessage = action("cached value");
+
+        WebTauStep step = WebTauStep.createStep(
+                tokenizedMessage(action("remove"), id(key), valueMessage),
+                () -> tokenizedMessage(action("removed"), id(key), valueMessage),
+                () -> fileBasedCache.remove(key));
+
+        step.execute(StepReportOptions.SKIP_START);
+    }
+
+    public boolean isExpired(String key, long expirationMs) {
+        MessageToken valueExpirationMessage = action("cache value expiration");
+        WebTauStep step = WebTauStep.createStep(
+                tokenizedMessage(action("check"), id(key), valueExpirationMessage),
+                (result) -> tokenizedMessage(action("checked"), id(key), valueExpirationMessage, COLON,
+                        classifier((boolean)result ? "expired" : "valid")),
+                () -> fileBasedCache.isExpired(key, expirationMs));
+
+        step.setInput(WebTauStepInputKeyValue.stepInput("expirationMs", expirationMs));
 
         return step.execute(StepReportOptions.SKIP_START);
     }
@@ -83,5 +131,44 @@ public class Cache {
                 });
 
         return step.execute(StepReportOptions.SKIP_START);
+    }
+
+    private <E, R> CachedValueAndMethod<R> getWithExpirationAndSupplierStep(String key, long expirationMs, Supplier<E> newValueSupplier, Function<E, R> converter) {
+        if (!exists(key)) {
+            E newValue = newValueSupplier.get();
+            put(key, newValue);
+
+            return new CachedValueAndMethod<>(ObtainMethod.CREATE_NEW, converter.apply(newValue));
+        } else if (isExpired(key, expirationMs)) {
+            E newValue = newValueSupplier.get();
+            put(key, newValue);
+
+            return new CachedValueAndMethod<>(ObtainMethod.RE_CREATE, converter.apply(newValue));
+        } else {
+            E existingValue = get(key);
+            return new CachedValueAndMethod<>(ObtainMethod.CACHED, converter.apply(existingValue));
+        }
+    }
+
+    enum ObtainMethod {
+        CREATE_NEW("created new value and cached"),
+        RE_CREATE("re-created value and cached"),
+        CACHED("got cached value");
+
+        private final String message;
+
+        ObtainMethod(String message) {
+            this.message = message;
+        }
+    }
+
+    static class CachedValueAndMethod<R> {
+        private final ObtainMethod method;
+        private final R value;
+
+        public CachedValueAndMethod(ObtainMethod method, R value) {
+            this.method = method;
+            this.value = value;
+        }
     }
 }
