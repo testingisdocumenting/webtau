bf1caae37f29e8a745efcb90023d553f61bc3183
Mykola Golubyev <golubev.nikolay@gmail.com>
2022-12-29 21:47:13 -0500

    groovyrunner: capture console outputs dir option (#1287)

--- webtau-console/src/main/java/org/testingisdocumenting/webtau/console/ConsoleOutputs.java
+++ webtau-console/src/main/java/org/testingisdocumenting/webtau/console/ConsoleOutputs.java
@@ -20,8 +20,10 @@ package org.testingisdocumenting.webtau.console;
 import org.testingisdocumenting.webtau.console.ansi.AnsiConsoleOutput;
 import org.testingisdocumenting.webtau.utils.ServiceLoaderUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class ConsoleOutputs {
@@ -30,6 +32,7 @@ public class ConsoleOutputs {
     private static final ConsoleOutput combined = new CombinedConsoleOutput();
 
     private static final List<ConsoleOutput> outputs = ServiceLoaderUtils.load(ConsoleOutput.class);
+    private static final ThreadLocal<List<ConsoleOutput>> localOutputs = ThreadLocal.withInitial(ArrayList::new);
 
     public static void out(Object... styleOrValues) {
         getOutputsStream().forEach(o -> o.out(styleOrValues));
@@ -51,7 +54,6 @@ public class ConsoleOutputs {
      */
     public static <E> void outLines(List<E> lines, Function<E, Object[]> styleOrValueExtractor) {
         getOutputsStream().forEach(o -> o.outLines(lines, styleOrValueExtractor));
-
     }
 
     /**
@@ -76,12 +78,29 @@ public class ConsoleOutputs {
         outputs.remove(consoleOutput);
     }
 
+    public static <R> R withAdditionalOutput(ConsoleOutput consoleOutput, Supplier<R> code) {
+        try {
+            addLocal(consoleOutput);
+            return code.get();
+        } finally {
+            removeLocal(consoleOutput);
+        }
+    }
+
+    private static void addLocal(ConsoleOutput output) {
+        localOutputs.get().add(output);
+    }
+
+    private static void removeLocal(ConsoleOutput output) {
+        localOutputs.get().remove(output);
+    }
+
     private static Stream<ConsoleOutput> getOutputsStream() {
         if (outputs.isEmpty()) {
             return Stream.of(defaultOutput);
         }
 
-        return outputs.stream();
+        return Stream.concat(outputs.stream(), localOutputs.get().stream());
     }
 
     private static class CombinedConsoleOutput implements ConsoleOutput {
