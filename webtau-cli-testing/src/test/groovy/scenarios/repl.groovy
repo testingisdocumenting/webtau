b84472fe895017965e087e69b41fc9ee13549bf2
mykolagolubyev <golubev.nikolay@gmail.com>
2023-02-11 14:06:59 -0500

    fix test

--- webtau-cli-testing/src/test/groovy/scenarios/repl.groovy
+++ webtau-cli-testing/src/test/groovy/scenarios/repl.groovy
@@ -68,7 +68,7 @@ scenario('http call') {
 
     cli.doc.capture('http-repl-output')
     fs.textContent(cfg.docArtifactsPath.resolve('http-repl-output/out.txt')).should containAll(
-            'header.statusCode', 'equals 200')
+            'header.statusCode', '200')
 }
 
 scenario('set config value') {
