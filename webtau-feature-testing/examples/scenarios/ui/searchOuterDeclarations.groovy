4d204d53f3b3a56a57f579b9aa1fb946cb262c68
Mykola Golubyev <golubev.nikolay@gmail.com>
2023-06-01 22:10:21 -0400

    browser: simplified page element doc page (#1495)

--- webtau-feature-testing/examples/scenarios/ui/searchOuterDeclarations.groovy
+++ webtau-feature-testing/examples/scenarios/ui/searchOuterDeclarations.groovy
@@ -2,13 +2,13 @@ package scenarios.ui
 
 import static org.testingisdocumenting.webtau.WebTauGroovyDsl.*
 
-def searchBox = $('#search-box')
+def searchBox = $("#search-box")
 def numberOfResults = searchBox.count
 
-scenario('search by specific query') {
-    browser.open('/search')
+scenario("search by specific query") {
+    browser.open("/search")
 
-    searchBox.setValue('search this')
+    searchBox.setValue("search this")
     searchBox.sendKeys("\n")
 
     numberOfResults.shouldBe > 1
