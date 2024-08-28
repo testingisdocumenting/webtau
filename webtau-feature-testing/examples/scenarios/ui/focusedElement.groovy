a88b896f29b6988f634c18678e439512f6314998
mykolagolubyev <golubev.nikolay@gmail.com>
2024-08-28 08:35:18 -0400

    update tests

--- webtau-feature-testing/examples/scenarios/ui/focusedElement.groovy
+++ webtau-feature-testing/examples/scenarios/ui/focusedElement.groovy
@@ -6,8 +6,9 @@ scenario("send text to focused element") {
     browser.open("/search")
     $("#search-box").click()
     // focused-element-send-keys
-    browser.focusedElement().sendKeys("search this\n")
+    browser.focusedElement().setValue("search this")
     // focused-element-send-keys
+    browser.focusedElement().sendKeys(browser.keys.enter)
 
     $("#message").should contain("searching for")
 }
